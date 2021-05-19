36ca4077595aac1a311fdb18ffa1482ffd47a5c8
cirobr <ciro.rosa@alumni.usp.br>
2021-05-18 21:09:43 -0300

    ep02 finalizado

--- ep02/ep02.py
+++ ep02/ep02.py
@@ -118,20 +118,24 @@ def normal_equation_weights(X, y):
     
     # START OF YOUR CODE:
     #raise NotImplementedError("Function normal_equation_weights() is not implemented")
-    
+
     import numpy as np
     from numpy.linalg import inv
     N = X.shape[0]
     #d = X.shape[1]
-    X_0 = np.ones((N,1))                # false X_0 coordinate
-    X_til = np.append(X_0, X, axis=1)   # insert false coordinate to X
+    
+    X_0 = np.ones((N,1))
+    X_til = np.append(X_0, X, axis=1)
     X_til = X_til.astype("float32")
-    Xt = X_til.T
-    X_aux = inv(np.dot(Xt, X_til))
-    X_cross = np.dot(X_aux, Xt)
-    w = np.dot(X_cross, y)
+    X_t = X_til.T
+    
+    X_a = np.matmul(X_t, X_til)
+    X_b = inv(X_a)
+    X_c = np.matmul(X_b, X_t)
+    w   = np.matmul(X_c, y)
     
     return(w)
+
     # END OF YOUR CODE
     
 # test of function normal_equation_weights()
@@ -165,11 +169,8 @@ def normal_equation_prediction(X, w):
     X_til = np.append(X_0, X, axis=1)   # insert false coordinate to X
     X_til = X_til.astype("float32")
 
-    y_hat = np.multiply(w.T, X_til)     # multiplica cada elemento de w.T por coluna em X_till
-    yh0 = y_hat[:,0].reshape((N, 1))
-    yh1 = y_hat[:,1:]
-    y_hat = yh0 + yh1
-    
+    y_hat = np.matmul(X_til, w)
+        
     return(y_hat)
 
     # END OF YOUR CODE
