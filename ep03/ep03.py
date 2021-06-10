8f1edbc3b0383904f1226335f0b181db0d2fd9dd
cirobr <ciro.rosa@alumni.usp.br>
2021-06-10 16:09:56 -0300

    Update ep03.py

--- ep03/ep03.py
+++ ep03/ep03.py
@@ -12,6 +12,7 @@ from sklearn.preprocessing import LabelEncoder
 
 from tensorflow.keras.datasets import mnist
 
+
 def prd(w, X, y):
     """
     computes the product of y.wt.X
@@ -24,7 +25,7 @@ def prd(w, X, y):
     return(res)
 
 
-# 1.1. Cross-entropy loss
+### 1.1. Cross-entropy loss
 def cross_entropy_loss(w, X, y):
     """
     Computes the loss (equation 1)
@@ -44,7 +45,7 @@ def cross_entropy_loss(w, X, y):
     return(loss)
 
 
-# 1.2. Gradient of the cross-entropy loss
+### 1.2. Gradient of the cross-entropy loss
 def cross_entropy_gradient(w, X, y):
     """
     Computes the gradient of the loss function (equation 2)
@@ -69,7 +70,7 @@ def cross_entropy_gradient(w, X, y):
     return(grad)
 
 
-# 1.3 Logistic regression training
+### 1.3 Logistic regression training
 def train_logistic(X, y, learning_rate = 1e-1, w0 = None,\
                         num_iterations = 1000, return_history = False):
     """
@@ -92,13 +93,52 @@ def train_logistic(X, y, learning_rate = 1e-1, w0 = None,\
     if w0 == None:
         w0 = np.random.normal(loc = 0, scale = 1, size = d)
 
+    cross_entropy_loss_history = np.empty(shape=(0,), dtype=float)
+    for t in range(num_iterations):
+        
+        if return_history:
+            cross_entropy_loss_history = np.append(cross_entropy_loss_history, \
+                                                   cross_entropy_loss(w0, X_one, y))
+
+        gt = cross_entropy_gradient(w0, X_one, y)
+        vt = -np.copy(gt)
+        wt = w0 + learning_rate * vt
+        w0 = np.copy(wt)
+        
+        # observação: considerar melhoria no critério de término do loop
+        # Mostafa, p.96
+        
+    if return_history:
+        res = (wt, cross_entropy_loss_history)
+    else:
+        res = wt
     
+    return(res)
 
 
+### 1.4. Logistic regression prediction
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
 
+def predict_logistic(X, w):
+    """
+    Computes the logistic regression prediction
+    :param X: design matrix
+    :type X: np.ndarray(shape=(N,d))
+    :param w: weight vector
+    :rtype: np.ndarray(shape=(1+d,))
+    :return: predicted classes 
+    :rtype: np.ndarray(shape=(N,))
+    """
+    
+    wt = np.transpose(w)
+    h = sigmoid(np.dot(wt, X))
+    
+    return (h)
 
 
+# 2.1. Generate two blobs of points
 # Create two blobs
 N = 300
 X, y = make_blobs(n_samples=N, centers=2, cluster_std=1, n_features=2, random_state=2)
@@ -108,7 +148,53 @@ y[y==0] = -1
 
 print("X.shape =", X.shape, "  y.shape =", y.shape)
 
-w = np.ones((2,))
-res = prd(w, X, y)
-grad = cross_entropy_gradient(w,X,y)
-train_logistic(X, y)
+
+# 2.2. Let's plot the blobs of points
+fig = plt.figure(figsize=(6,6))
+
+# plot negatives in red
+plt.scatter(X[y==-1,0], \
+            X[y==-1,1], \
+            alpha = 0.5,\
+            c = 'red')
+
+# and positives in blue
+plt.scatter(x=X[y==1,0], \
+            y=X[y==1,1], \
+            alpha = 0.5, \
+            c = 'blue')
+
+P=+1
+N=-1
+legend_elements = [ Line2D([0], [0], marker='o', color='r',\
+                    label='Class %d'%N, markerfacecolor='r',\
+                    markersize=10),\
+                    Line2D([0], [0], marker='o', color='b',\
+                    label='Class %d'%P, markerfacecolor='b',\
+                    markersize=10) ]
+
+plt.legend(handles=legend_elements, loc='best')
+plt.show()
+
+
+### 2.3. Let's train the linear regressor and plot the loss curve
+np.random.seed(567)
+
+# ==> Replace the right hand side below with a call to the
+# train_logistic() function defined above. Use parameter return_history=True
+
+#w_logistic, loss = np.array([0,0,0]), [0]
+w_logistic, loss = train_logistic(X, y)
+
+# ==> Your code insert ends here
+
+print()
+print("Final weight:\n", w_logistic)
+print()
+print("Final loss:\n", loss[-1])
+
+plt.figure(figsize = (12, 8))
+plt.plot(loss)
+plt.xlabel('Iteration #')
+plt.ylabel('Cross Entropy Loss')
+plt.show()
